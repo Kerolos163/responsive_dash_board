ce561a913bbbe33d389c0fa092ae9f51105afc16
kerolos <73710150+Kerolos163@users.noreply.github.com>
2024-01-18 01:58:46 +0200

    finish AppStyles

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  static const styleRegular12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: Color(0xffAAAAAA),
+  );
+
+  static const styleRegular14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: Color(0xffAAAAAA),
+  );
+
+  static const styleRegular16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: Color(0xff064060),
+  );
+
+  static const styleMedium16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: Color(0xff064060),
+  );
+
+  static const styleSemiBold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    color: Color(0xff064060),
+  );
+  static const styleBold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    color: Color(0xff4AB7f2),
+  );
+
+  static const styleSemiBold18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    color: Color(0xffFFFFFF),
+  );
+
+  static const styleSemiBold20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: Color(0xff064060),
+  );
+
+  static const styleMedium20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    color: Color(0xffFFFFFF),
+  );
+
+  static const styleSemiBold24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    color: Color(0xff4AB7f2),
+  );
+}
