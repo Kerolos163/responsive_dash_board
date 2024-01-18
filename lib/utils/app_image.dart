7a27ff9de52d22da01360cb1144d418bb7f9fb93
kerolos <73710150+Kerolos163@users.noreply.github.com>
2024-01-18 20:13:37 +0200

    Add assets and Font

---
+++ lib/utils/app_image.dart
@@ -0,0 +1,52 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAvatar1
+  /// assets/images/avatar1.svg
+  static const String imagesAvatar1 = "assets/images/avatar1.svg";
+
+  /// Assets for imagesAvatar2
+  /// assets/images/avatar2.svg
+  static const String imagesAvatar2 = "assets/images/avatar2.svg";
+
+  /// Assets for imagesBalance
+  /// assets/images/Balance.svg
+  static const String imagesBalance = "assets/images/Balance.svg";
+
+  /// Assets for imagesCardDashBoard
+  /// assets/images/CardDashBoard.svg
+  static const String imagesCardDashBoard = "assets/images/CardDashBoard.svg";
+
+  /// Assets for imagesDashboard
+  /// assets/images/Dashboard.svg
+  static const String imagesDashboard = "assets/images/Dashboard.svg";
+
+  /// Assets for imagesExpenses
+  /// assets/images/Expenses.svg
+  static const String imagesExpenses = "assets/images/Expenses.svg";
+
+  /// Assets for imagesIncome
+  /// assets/images/income.svg
+  static const String imagesIncome = "assets/images/income.svg";
+
+  /// Assets for imagesMyInvestments
+  /// assets/images/My_Investments.svg
+  static const String imagesMyInvestments = "assets/images/My_Investments.svg";
+
+  /// Assets for imagesMyTransaction
+  /// assets/images/My_Transaction.svg
+  static const String imagesMyTransaction = "assets/images/My_Transaction.svg";
+
+  /// Assets for imagesSetting
+  /// assets/images/setting.svg
+  static const String imagesSetting = "assets/images/setting.svg";
+
+  /// Assets for imagesStatistics
+  /// assets/images/Statistics.svg
+  static const String imagesStatistics = "assets/images/Statistics.svg";
+
+  /// Assets for imagesWallet2
+  /// assets/images/wallet-2.svg
+  static const String imagesWallet2 = "assets/images/wallet-2.svg";
+}
+
